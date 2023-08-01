65bc03424446df09ccfcbbcb5f34c8c34acfbc5a
Calin G <calingoia01@yahoo.com>
2023-08-01 22:47:29 +0300

    add events_list_item add glide

--- app/src/main/java/com/example/tmsandroid/EventsRecViewAdapter.java
+++ app/src/main/java/com/example/tmsandroid/EventsRecViewAdapter.java
@@ -1,20 +1,30 @@
 package com.example.tmsandroid;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class EventsRecViewAdapter extends RecyclerView.Adapter<EventsRecViewAdapter.ViewHolder> {
 
     private ArrayList<Event> events = new ArrayList<>();
+    private Context context;
 
-    public EventsRecViewAdapter() {}
+    public EventsRecViewAdapter(Context context) {
+        this.context = context;
+    }
 
     @NonNull
     @Override
@@ -26,7 +36,21 @@ public class EventsRecViewAdapter extends RecyclerView.Adapter<EventsRecViewAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
         holder.txtName.setText(events.get(position).getEventName());
+        holder.txtID.setText(String.valueOf(events.get(position).getEventID()));
+
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, events.get(position).getEventName() + " selected!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Glide.with(context)
+                .asBitmap()
+                .load(events.get(position).getImageUrl())
+                .into(holder.image);
     }
 
     @Override
@@ -44,11 +68,15 @@ public class EventsRecViewAdapter extends RecyclerView.Adapter<EventsRecViewAdap
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        private TextView txtName;
-
+        private TextView txtName, txtID;
+        private CardView parent;
+        private ImageView image;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName = itemView.findViewById(R.id.txtName);
+            parent = itemView.findViewById(R.id.parent);
+            txtID = itemView.findViewById(R.id.txtID);
+            image = itemView.findViewById(R.id.image);
         }
     }
 
--- app/src/main/java/com/example/tmsandroid/MainActivity.java
+++ app/src/main/java/com/example/tmsandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tmsandroid;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,9 +21,9 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Event> events = new ArrayList<>();
         events.add(new Event(1,"Untold","Festival de Muzica", "https://i0.1616.ro/media/2/2701/33647/21212741/1/untold.png"));
         events.add(new Event(2, "RockFestival", "Festival de Rock", "https://www.institutiilestatului.ro/wp-content/uploads/2023/06/355694001_658527916301559_2796432390177616914_n.jpg"));
-       EventsRecViewAdapter adapter = new EventsRecViewAdapter();
+       EventsRecViewAdapter adapter = new EventsRecViewAdapter(this);
        adapter.setEvents(events);
        eventsRecView.setAdapter(adapter);
-       eventsRecView.setLayoutManager(new LinearLayoutManager(this));
+       eventsRecView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
