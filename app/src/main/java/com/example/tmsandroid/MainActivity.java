610d0bcb098de61ee54fb1bf617f4a6e743b3831
Calin G <calingoia01@yahoo.com>
2023-08-01 18:43:23 +0300

    add style.xml, material design dependency

--- app/src/main/java/com/example/tmsandroid/MainActivity.java
+++ app/src/main/java/com/example/tmsandroid/MainActivity.java
@@ -11,24 +11,10 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView textView;
-    private EditText editText;
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.buttonID) {
-            Toast.makeText(this,"Btn clicked", Toast.LENGTH_SHORT).show();
-            textView.setText("Hello " + editText.getText().toString());
-        }
-    }
-
+public class MainActivity extends AppCompatActivity{
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnHello = findViewById(R.id.buttonID);
-        btnHello.setOnClickListener(this);
-        editText = findViewById(R.id.textNameID);
-        textView = findViewById(R.id.textViewID);
     }
 }
