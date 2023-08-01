847ee5a21a469bc587d1f0f71ac2ed71396aec06
Calin G <calingoia01@yahoo.com>
2023-08-01 16:59:19 +0300

    add EditText, Button, TextView for testing purpose

--- app/src/main/java/com/example/tmsandroid/MainActivity.java
+++ app/src/main/java/com/example/tmsandroid/MainActivity.java
@@ -1,4 +1,34 @@
 package com.example.tmsandroid;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView textView;
+    private EditText editText;
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonID) {
+            Toast.makeText(this,"Btn clicked", Toast.LENGTH_SHORT).show();
+            textView.setText("Hello " + editText.getText().toString());
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnHello = findViewById(R.id.buttonID);
+        btnHello.setOnClickListener(this);
+        editText = findViewById(R.id.textNameID);
+        textView = findViewById(R.id.textViewID);
+    }
 }
